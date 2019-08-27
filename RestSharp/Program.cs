12a08e66608347c55b548ed4d6251c19978a12b0
ElenQa <47454865+ElenQa@users.noreply.github.com>
2019-08-27 13:57:19 +0300

    Add project files.

---
+++ RestSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+
+
+namespace RestSharp_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://gogotaxi.com.ua");
+
+            RestRequest request = new RestRequest(Method.GET);
+            request.Resource = "/api/driver/me";
+            request.AddHeader("token", "ce18c8257503ba0dfa0656ea4306d22b3933ffbe1923aaff18ecc078b5070a52");
+
+            IRestResponse response = client.Execute(request);
+            string content = response.Content;
+
+            object desContent = JsonConvert.DeserializeObject(content);
+
+            Console.Write(desContent);
+            Console.ReadKey();
+        }
+    }
+}
